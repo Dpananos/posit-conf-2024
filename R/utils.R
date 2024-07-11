3a0401d3da9c964d64f9a208f291163698f40c26
Demetri Pananos <dpananos@gmail.com>
2024-07-10 23:48:42 -0400

    adds more slides

--- R/utils.R
+++ R/utils.R
@@ -1,52 +1,22 @@
 
 create_experiment_data <- function() {
   
-  set.seed(2)
-  device <- c('Desktop', 'Mobile')
-  treat <- c('Control', 'Treatment')
+  set.seed(3)
+
   
-  
-  dgrid <- crossing(
-    device, 
-    treat
-  ) %>% 
-    mutate(wt = c(0.4, 0.4, 0.1, 0.1))
-  
-  grid <- dgrid %>% 
-    sample_n(20000, replace=T, weight = wt) %>% 
+  d <- readr::read_csv('data/data.csv') %>% 
+    sample_n(size = 40000, weight = weight, replace = TRUE) %>% 
+    group_by(device, lang) %>% 
     mutate(
-      tenure = pmin(6.0, rpois(n(), 2.5))
+      treat = sample(c("Treatment","Control"), size = n(), replace = T),
+      p = if_else(treat == 'Treatment', p_treat, p_control),
+      conversion = rbinom(n(), 1, p)
     )
   
-  X <- model.matrix( ~ device*treat + treat*splines::ns(tenure,Boundary.knots = c(2, 5), knots = c(2.5, 4.5)),
-                     data=grid
-                    )
   
-
-  b <- c(0.10, -0.03, 0.015, 
-         -1/20, -1/20, -1/20, 
-         -0.013,
-         -0.005, -0.005, -0.005)
+  d
   
   
-  
-  
- grid %>% 
-   mutate(
-     p = as.numeric(X %*% b),
-     conversion = rbinom(n(), 1, p)
-   ) 
   
 }
 
-
-summarized_experiment_data <- function(rollup_vars=NULL) {
-  
-  create_experiment_data() %>% 
-    group_by(treat, across({{rollup_vars}})) %>% 
-    summarise(
-      N = n(),
-      conversions = sum(conversion)
-    )
-  
-}
